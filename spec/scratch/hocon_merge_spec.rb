6d14ed3f5f16c0ad97e1770039eb8c4ab484c5bd
Chris Price <chris@puppetlabs.com>
2016-05-11 20:50:20 -0700

    WIP: add crude run mode support

--- spec/scratch/hocon_merge_spec.rb
+++ spec/scratch/hocon_merge_spec.rb
@@ -4,21 +4,42 @@ require 'hocon/config_factory'
 require 'hocon/config_resolve_options'
 
 class HoconLoader
-  def self.parse(defaults_file, user_file)
+  def self.parse(defaults_file, user_file, run_mode)
     user_config = Hocon::ConfigFactory.parse_file(user_file)
     default_config = Hocon::ConfigFactory.parse_file(defaults_file)
 
-    Hocon::ConfigFactory.
+    resolved_config = Hocon::ConfigFactory.
         load_from_config(user_config.with_fallback(default_config),
                          Hocon::ConfigResolveOptions.defaults).root.unwrapped
+
+    # filter out any nested maps to give us just the resolved "main" settings,
+    # since we know that Puppet only supports scalar settings values, and our
+    # goal is to overlay the run-mode settings onto the main settings.
+    main_config = resolved_config.reject do |k, v|
+      v.instance_of?(Hash)
+    end
+
+    # Now grab the relevant settings for the specified run mode
+    run_mode_config = resolved_config[run_mode]
+
+    # finally, merge the run_mode settings onto the "main" settings.
+    main_config.merge(run_mode_config)
   end
 end
 
 describe "hocon merge with interpolation" do
-  it "should work" do
-    expect(HoconLoader.parse("./spec/scratch/config_files/default.conf",
-                             "./spec/scratch/config_files/user.conf")).
+  defaults_file = "./spec/scratch/config_files/default.conf"
+  user_file = "./spec/scratch/config_files/user.conf"
+
+  it "should load master runmode settings" do
+    expect(HoconLoader.parse(defaults_file, user_file, "master")).
         to eq({"vardir" => "./my-vardir",
                "reportdir" => "./my-vardir/reports"})
+  end
+
+  it "should load agent runmode settings" do
+    expect(HoconLoader.parse(defaults_file, user_file, "agent")).
+        to eq({"vardir" => "./my-vardir",
+               "reportdir" => "./my-vardir/agent-reports"})
   end
 end
